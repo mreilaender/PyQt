1295b9bff6bd4d163c8e1442c1f6c747596bd68d
BuildTools <mreilaender@student.tgm.ac.at>
2015-12-21 18:31:16 +0100

    implemented reset

--- src/model.py
+++ src/model.py
@@ -1,3 +1,4 @@
+from src.ButtonHandler import ButtonHandler
 from src.CheckOrder import CheckOrder
 from src.PlayerStats import PlayerStats
 
@@ -10,18 +11,32 @@ class Model(object):
     def __init__(self):
         self.check = CheckOrder(1)
         self.stats = None
+        self.buttons = ButtonHandler()
 
     def on_button_pressed(self, button):
         # if self.check.check_order(1):
         #     return False
         if self.stats is None:
-            print("Labens not set")
+            print("Labels not set")
             return False
         else:
+            self.stats.increase_total()
             if self.check.check_order(int(button.text())):
-                print(self.stats.stats["left"])
+                self.stats.increase_correct()
                 self.stats.decrease_left()
+                button.setEnabled(False)
+                self.buttons.add_button(button)
                 self.check.increment()
+            else:
+                self.stats.increase_wrong()
 
     def set_labels(self, left, correct, wrong, total, games):
         self.stats = PlayerStats(left, correct, wrong, total, games)
+
+    def set_default_values(self, left=None, correct=None, wrong=None, total=None):
+        self.stats.set_default_values(left, correct, wrong, total)
+
+    def reset(self):
+        self.stats.reset()
+        self.buttons.reset()
+        self.check.reset()
